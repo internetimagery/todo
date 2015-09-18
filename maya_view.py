00c93c71a3264f73c434a747c0a11d2fd688aecc
Internetimagery <jason.dixon.email@gmail.com>
2015-09-19 01:16:50 +1200

    adding todo element to gui

--- maya_view.py
+++ maya_view.py
@@ -30,17 +30,19 @@ class GUIElement(object):
 # options:
 # title = window title
 # location = "float" or "left" or "right"
-# moveCallback = run when docked or undocked
+# moveCallback(location) = run when docked or undocked
 # closeCallback = run when window closed
+# buildTodoCallback(parent) = run when switching to main todo window
+# buildSettingsCallback(parent) = run when switching to settings window
+# newTodoCallback(text) = run when creating a new todo
 class MainWindow(GUIElement):
     """
     build window
     """
     def buildElement(s):
-        s.removeUI()
         window = cmds.window(title=title, rtf=True)
         s.outerContainer = cmds.columnLayout(adj=True)
-        s.wrapper = cmds.dockControl(
+        s.window = cmds.dockControl(
             s.label,
             a="float",
             content=window,
@@ -52,7 +54,57 @@ class MainWindow(GUIElement):
             )
         if s.options["location"] in ["left", "right"]:
             cmds.dockControl(s.wrapper, e=True, a=s.options["location"], fl=False)
+        s.buildTodo()
         return s.wrapper
+
+    """
+    Build todo window
+    """
+    def buildTodo(s):
+        s.removeUI()
+        s.wrapper = cmds.columnLayout(adj=True)
+        cmds.columnLayout(adj=True)
+        cmds.iconTextButton(
+            h=30,
+            ann="Click to view the Todo scripts settings. Settings are saved with the Maya scene, so you will need to set them for each scene.",
+            image="attributes.png",
+            label="Settings ->",
+            style="iconAndTextHorizontal",
+            c=s.buildSettings
+            )
+        cmds.separator()
+        text = cmds.textField(
+            aie=True,
+            ed=True,
+            h=30,
+            ann="Type a task into the box.",
+            ec=lambda x: s.options["newTodoCallback"](cmds.textField(text, q=True, tx=True))
+            )
+        cmds.button(
+            label="Create a new TODO",
+            h=20,
+            ann="Type a task into the box.",
+            c=lambda x: s.options["newTodoCallback"](cmds.textField(text, q=True, tx=True)
+            )
+        cmds.setParent("..")
+
+    """
+    Build settings window
+    """
+    def buildSettings(s):
+        s.removeUI()
+        s.wrapper = cmds.columnLayout(adj=True, p=s.outerContainer)
+        cmds.iconTextButton(
+            h=30,
+            ann="Click to return to your Todo list.",
+            image="revealSelected.png",
+            label="<- Todo",
+            style="iconAndTextHorizontal",
+            c=s.buildTodo
+            )
+        cmds.separator()
+        cmds.text(label="Settings are unique to each Maya scene.", h=50)
+        s.options["buildSettingsCallback"]()
 
     """
     Keep track of dock movement
@@ -77,7 +129,7 @@ class MainWindow(GUIElement):
             cmds.scriptJob(ie=s.closeDock, p=s.wrapper, ro=True)
         elif not visible:
             print "Window closed."
-            s.removeUI()
+            cmds.deleteUI(s.window)
             s.options["closeCallback"]()
 
 # options:
