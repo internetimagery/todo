f3e53152ecd12350e94398e371094e98522e98c8
Jason <jason.dixon.email@gmail.com>
2015-10-07 03:42:57 +1300

    adding callback for refresh

--- view/c4d/window.py
+++ view/c4d/window.py
@@ -4,12 +4,15 @@ from c4d import gui
 
 class Window(gui.GeDialog):
 
-    def __init__(s, requestSettingsPage, requestTodoPage):
+    def __init__(s, requestSettingsPage, requestTodoPage, requestReset):
         s.elements = {} # Keep track of elements and their associative functions
         s.requestSettingsPage = requestSettingsPage # Callback to get settings built
         s.requestTodoPage = requestTodoPage # Callback to get todos information
+        s.requestReset = requestReset # Callback when scene changes asking for a data reset
         s.todoIds = [] # Store the ids of Todos, to free them up
+        s.panelIds = [] # Store ids for each panel
         s.lastDoc = None # Previous document, to detect scene change
+        s.CoreMessage(c4d.EVMSG_DOCUMENTRECALCULATED, None) # Trigger first refresh
         gui.GeDialog.__init__(s)
 
     def Open(s):
@@ -21,14 +24,15 @@ class Window(gui.GeDialog):
             )
 
     def CreateLayout(s):
-        s.SetTitle("Todo window")
-        s.TabGroupBegin(
-            id=s.getId(),
-            flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT
+        s.SetTitle("Todo window") # TODO add a way for this info to be provided
+        s.wrapper = s.getId()
+        s.GroupBegin( # Open Page
+            id=s.wrapper,
+            flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
+            cols=1
             )
-        s.buildTodoPage(**s.requestTodoPage())
-        s.buildSettingsPage(**s.requestSettingsPage())
         s.GroupEnd()
+        s.buildTodoPage(**s.requestTodoPage())
         return True
 
     def Command(s, id, msg):
@@ -46,8 +50,8 @@ class Window(gui.GeDialog):
                 currPath if currPath else ""
                 )
             if s.lastDoc != identifier:
-                print "Scene Changed!"
                 s.lastDoc = identifier
+                s.requestReset()
         return True
 
     def buildTodoPage(s, buttonName="Default", pageName="Default", newTodo=None, buildTodo=None):
@@ -56,19 +60,33 @@ class Window(gui.GeDialog):
         newTodo = callback when new todo is requested
         buildTodo = call to build out todo list based on given information dict
         """
+        if s.panelIds:
+            for id in s.panelIds:
+                s.unbind(id)
+        s.LayoutFlushGroup(id=s.wrapper) # Empty the panel
         s.GroupBegin( # Open Page
-            id=s.getId(),
+            id=0,
             flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
             cols=1,
             title=pageName,
             groupflags=c4d.BFV_CMD_EQUALCOLUMNS
             )
+        pageBtn = s.getId()
+        s.panelIds.append(pageBtn)
+        s.AddButton(
+            id=pageBtn,
+            flags=c4d.BFH_RIGHT,
+            name="Settings ->"
+            )
+        s.bind(pageBtn, lambda x: s.buildSettingsPage(**s.requestSettingsPage()))
         textfield = s.getId()
+        s.panelIds.append(textfield)
         s.AddEditText(
             id=textfield,
             flags=c4d.BFH_SCALEFIT,
         )
         button = s.getId()
+        s.panelIds.append(button)
         s.AddButton(
             id=button,
             flags=c4d.BFH_SCALEFIT,
@@ -76,11 +94,12 @@ class Window(gui.GeDialog):
             )
         s.AddSeparatorH(c4d.BFH_SCALEFIT)
         s.ScrollGroupBegin( # Open Scroll
-            id=s.getId(),
+            id=0,
             flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
             scrollflags=c4d.SCROLLGROUP_VERT
             )
         s.todolist = s.getId()
+        s.panelIds.append(s.todolist)
         s.GroupBegin( # Open Dummy
             id=s.todolist,
             flags=c4d.BFH_FIT|c4d.BFV_TOP,
@@ -90,6 +109,8 @@ class Window(gui.GeDialog):
         s.GroupEnd() # Close Dummy
         s.GroupEnd() # Close Scroll
         s.GroupEnd() # Close Page
+        s.LayoutChanged(id=s.wrapper)
+
         def new(id):
             if newTodo:
                 if newTodo(s.GetString(textfield)):
@@ -152,6 +173,10 @@ class Window(gui.GeDialog):
         """
         Build the todo page
         """
+        if s.panelIds:
+            for id in s.panelIds:
+                s.unbind(id)
+        s.LayoutFlushGroup(id=s.wrapper) # Empty the panel
         s.GroupBegin( # Open Page
             id=s.getId(),
             flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
@@ -159,8 +184,24 @@ class Window(gui.GeDialog):
             title="Settings",
             groupflags=c4d.BFV_CMD_EQUALCOLUMNS
             )
+        pageBtn = s.getId()
+        s.panelIds.append(pageBtn)
+        s.AddButton(
+            id=pageBtn,
+            flags=c4d.BFH_LEFT,
+            name="<- Tasks"
+            )
+        s.AddSeparatorH(c4d.BFH_SCALEFIT)
+        s.AddStaticText(
+            0,
+            c4d.BFH_CENTER,
+            name="Settings are Scene Independant."
+        )
+        s.AddSeparatorH(c4d.BFH_SCALEFIT)
+        s.bind(pageBtn, lambda x: s.buildTodoPage(**s.requestTodoPage()))
         s.AddButton(1013, c4d.BFV_MASK, initw=145, name="INSERT SETTINGS IN HERE LATER")
         s.GroupEnd() # Close Page
+        s.LayoutChanged(id=s.wrapper)
 
     def buildImageButton(s, image, text):
         bmpbutton = c4d.BaseContainer()
@@ -175,7 +216,8 @@ class Window(gui.GeDialog):
             flags=0,
             minw=0,
             minh=0,
-            customdata=bmpbutton)
+            customdata=bmpbutton
+            )
         return id
 
     def getId(s):
@@ -196,6 +238,8 @@ class Window(gui.GeDialog):
 
 if __name__ == '__main__':
     # TEST THE WINDOW
+    def resetScene():
+        print "Scene changed. Need to load more info"
     def infoTodo(): # fill in the information and give it to the GUI
         return {
             "buttonName"  : "Create a new TODO",
@@ -213,6 +257,7 @@ if __name__ == '__main__':
 
     w = Window(
         lambda: {},
-        infoTodo
+        infoTodo,
+        resetScene
     )
     w.Open()
