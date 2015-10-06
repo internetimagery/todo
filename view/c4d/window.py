b9a729e9e4c8468d5ddcf99b5f903a7107ecc7b5
Jason <jason.dixon.email@gmail.com>
2015-10-07 00:00:08 +1300

    capture new todo text

--- view/c4d/window.py
+++ view/c4d/window.py
@@ -5,7 +5,9 @@ from c4d import gui
 class Window(gui.GeDialog):
 
     def __init__(s):
-        s.events = {}
+        s.elements = {} # Keep track of elements and their associative functions
+        s.requestSettings = None # Callback to get settings built
+        s.requestTodos = None # Callback to get todos information
         gui.GeDialog.__init__(s)
 
     def Open(s):
@@ -38,8 +40,21 @@ class Window(gui.GeDialog):
             title="Tasks",
             groupflags=c4d.BFV_CMD_EQUALCOLUMNS
             )
-        s.AddButton(1013, c4d.BFV_MASK, initw=145, name="Tab one")
+        textfield = s.getId()
+        button = s.getId()
+        s.AddEditText(
+            id=textfield,
+            flags=c4d.BFH_SCALEFIT,
+        )
+        s.AddButton(
+            id=button,
+            flags=c4d.BFH_SCALEFIT,
+            name="ADD A TODO IN HERE, ALSO REPLACE THIS TEXT"
+            )
         s.GroupEnd()
+        def newTask(id):
+            print s.GetString(textfield)
+        s.bind(button, newTask)
 
     def buildSettings(s):
         """
@@ -52,29 +67,29 @@ class Window(gui.GeDialog):
             title="Settings",
             groupflags=c4d.BFV_CMD_EQUALCOLUMNS
             )
-        s.AddButton(1013, c4d.BFV_MASK, initw=145, name="Tab two")
+        s.AddButton(1013, c4d.BFV_MASK, initw=145, name="INSERT SETTINGS IN HERE LATER")
         s.GroupEnd()
 
     def Command(s, id, msg):
-        if id in s.events and s.events[id]:
-            s.events[id]()
+        if id in s.elements and s.elements[id]:
+            s.elements[id](id)
         return True
 
     def getId(s):
         start = 1050
-        ids = s.events.keys()
+        ids = s.elements.keys()
         while True:
             if start not in ids:
-                s.events[start] = None
+                s.elements[start] = None
                 return start
             start += 1
 
     def bind(s, id, function):
-        s.events[id] = function
+        s.elements[id] = function
 
     def unbind(s, id):
-        if id in s.events:
-            del s.events[id]
+        if id in s.elements:
+            del s.elements[id]
 
 if __name__ == '__main__':
     w = Window()
